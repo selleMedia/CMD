923bf78a359265f73025e5cd3ec9573e45e11b7a
jg_0716c29s <jg@sell-e.com>
2016-09-14 12:03:41 +0200

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_hC3XXss0p_revisr.sql
+++ wp-content/uploads/revisr-backups/revisr_hC3XXss0p_revisr.sql
@@ -19,12 +19,12 @@ CREATE TABLE `hC3XXss0p_revisr` (
   `event` varchar(42) NOT NULL,
   `user` varchar(60) DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `hC3XXss0p_revisr` WRITE;
 /*!40000 ALTER TABLE `hC3XXss0p_revisr` DISABLE KEYS */;
-INSERT INTO `hC3XXss0p_revisr` VALUES (1,'2016-09-14 11:44:37','Successfully created a new repository.','init','jg_0716c29s'),(2,'2016-09-14 11:57:58','Successfully backed up the database.','backup','jg_0716c29s'),(3,'2016-09-14 11:58:00','There was an error committing the changes to the local repository.','error','jg_0716c29s');
+INSERT INTO `hC3XXss0p_revisr` VALUES (1,'2016-09-14 11:44:37','Successfully created a new repository.','init','jg_0716c29s'),(2,'2016-09-14 11:57:58','Successfully backed up the database.','backup','jg_0716c29s'),(3,'2016-09-14 11:58:00','There was an error committing the changes to the local repository.','error','jg_0716c29s'),(4,'2016-09-14 11:58:51','Successfully backed up the database.','backup','jg_0716c29s'),(5,'2016-09-14 11:58:51','Committed <a href=\"https://www.chiromd.de/wp-admin/admin.php?page=revisr_view_commit&commit=73892a8&success=true\">#73892a8</a> to the local repository.','commit','jg_0716c29s'),(6,'2016-09-14 11:58:52','Error pushing changes to the remote repository.','error','jg_0716c29s');
 /*!40000 ALTER TABLE `hC3XXss0p_revisr` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
